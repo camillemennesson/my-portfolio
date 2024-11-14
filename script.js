d388fbfb715479c9bd96313ff572d7caed5ad474
camillemennesson <camille.mennesson@gmail.com>
2024-11-14 15:52:10 +0100

    TEST

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.addEventListener('mousemove', (e) => {
 });
 
 
+//NAVBAR MENU
         function toggleMenu() {
             const navLinks = document.getElementById('nav-links');
             navLinks.classList.toggle('active');
@@ -19,7 +20,7 @@ document.addEventListener('mousemove', (e) => {
     });
 });
 
-
+// BACK TO TOP BUTTON
 // Get the button
 const backToTopButton = document.getElementById('backToTopButton');
 
