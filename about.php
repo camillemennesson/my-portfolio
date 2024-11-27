55bb2a50f47ec8b9105387b5568b6c2b12ef6499
camillemennesson <camille.mennesson@gmail.com>
2024-11-27 20:20:59 +0100

    update about.php and style.css

--- about.php
+++ about.php
@@ -16,7 +16,6 @@ function auto_version($file) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About</title>
-
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo auto_version('style.css'); ?>" type="text/css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet" />
