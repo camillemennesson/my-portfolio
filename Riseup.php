ee3153dd9c6539a1c4cc35cc416aa0906a8bcb98
camillemennesson <camille.mennesson@gmail.com>
2025-10-04 19:47:43 +0200

    `Refactored Solutions section in Riseup.php, removing or modifying several bullet points.`

--- Riseup.php
+++ Riseup.php
@@ -187,30 +187,22 @@
         <h3>Solutions</h3>
         <ul class="key-challenges">
             <li><b>Clearer Navigation</b>
-            Simplified and restructured the navigation menu to ensure clear hierarchies and intuitive categories.
-            This helped reduce friction, improved content discoverability, and created a smoother user journey 
-            from homepage to conversion pages.            
+            Simplified and restructured the navigation menu to ensure clear hierarchies and intuitive categories.         
             </li>
             <li><b>Transparency Boost</b>
-            One pain point identified was a lack of trust signals and clarity around the product offering.
-             To address this, I introduced clearer pricing visibility, transparent messaging, and highlighted 
-             social proof elements like client logos and testimonials.
+           Introduced clearer pricing visibility, transparent messaging, and highlighted social proof elements.
             </li>        
             <li><b>Scalable UI System</b>
-            The old website design lacked consistency and flexibility, making it challenging to maintain as the brand evolved. 
-            I developed a scalable UI system with reusable components, typography rules, and a cohesive color palette aligned 
+            Developed a scalable UI system with reusable components, typography rules, and a cohesive color palette aligned 
             with Rise Up’s new branding.
             </li>
             <li><b>Key pages flow</b>
-            The old user flow between key pages—such as Solutions, Pricing, and Demo Request—was fragmented and unclear. 
-            I restructured the content and interactions to create a smoother flow, guiding visitors logically from learning 
+            Restructured the content and interactions to create a smoother flow, guiding visitors logically from learning 
             about the platform to requesting a demo.           
              </li>
             <li><b>Highlighting USPs</b>
-            Rise Up’s unique value propositions were buried in the previous website design. 
-            I worked on bringing them to the forefront with dedicated sections, bold typography, and engaging visuals.
-            By clearly highlighting what makes Rise Up different—such as AI-powered learning and adaptability—the website
-            now communicates the brand’s strengths more effectively, increasing its impact on potential customers.            
+            Worked on bringing unique selling points to the forefront with dedicated sections, bold typography, and engaging visuals.
+                  
             </li>
         </ul>
         <div class="image-item">
